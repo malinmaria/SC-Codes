ff60b1eabc73220bab2c88c8ad08a7e1205b8c04
malinmaria <malin.nemergut@gmail.com>
2020-10-21 19:56:57 -0400

    setting up jQuery functions

---
+++ Dragon Calculator/scripts.js
@@ -0,0 +1,16 @@
+function calculateBudget() {
+  //runs when button is clicked
+}
+
+function validForm() {
+
+}
+
+function openModal(content) {
+  //modal handling goes here
+  //takes a string as an arg to know what to display
+}
+
+function closeModal() {
+  
+}
