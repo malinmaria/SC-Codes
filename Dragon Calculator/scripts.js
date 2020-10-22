d4be9e3ceb3548b995e26f7e689ba7fefad24271
malinmaria <malin.nemergut@gmail.com>
2020-10-21 21:34:06 -0400

    editing validForm fn

--- Dragon Calculator/scripts.js
+++ Dragon Calculator/scripts.js
@@ -1,9 +1,18 @@
 function calculateBudget() {
   //runs when button is clicked
+  alert('Calculations!');
 }
 
 function validForm() {
+  var numberOfFields = $('form input').length,
+  numberOfValidFields = 0;
 
+  $('form input').each(function (){
+    if ($(this).val().length !== 0){
+      numberOfValidFields += 1
+    }
+  });
+  return numberOfFields === numberOfValidFields;
 }
 
 function openModal(content) {
@@ -12,5 +21,7 @@ function openModal(content) {
 }
 
 function closeModal() {
-  
+
 }
+
+$('form a.calculate').on('click', calculateBudget);
